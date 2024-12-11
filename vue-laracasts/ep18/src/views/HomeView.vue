a56b1c65c988ed55df4145546439c21ce3442366
mathdothings <jhonata.prodrigues@gmail.com>
2024-12-10 22:58:30 -0300

    feat: compositions API

---
+++ vue-laracasts/ep18/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+let message = ref('Hello, world!')
+
+function doIt() {
+  alert('Done!')
+}
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    {{ message }}
+    <div>
+      <input type="text" v-model="message" />
+    </div>
+    <div>
+      <button @click="doIt">Click</button>
+    </div>
+  </main>
+</template>
